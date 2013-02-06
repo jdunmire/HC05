011d1e26a7d3f71a916524c149466aeee4f73dcb
Jerry Dunmire <jerry_dunmire@fake.fake>
2013-02-06 14:27:49 -0800

    Test program now demonstrates baud changes
    BT connection must be re-established after each rate change.

--- Examples/hc05_test/hc05_test.ino
+++ Examples/hc05_test/hc05_test.ino
@@ -3,9 +3,9 @@
 #include <SoftwareSerial.h>
 
 #ifdef HC05_SOFTWARE_SERIAL
-HC05 myHC05 = HC05(3, 2, 4, 5);
+HC05 myHC05 = HC05(3, 2, 4, 5);  // cmd, state, rx, tx
 #else
-HC05 myHC05 = HC05(3, 2);
+HC05 myHC05 = HC05(3, 2);  // cmd, state
 #endif
 
 #ifdef DEBUG_HC05
@@ -16,68 +16,41 @@ HC05 myHC05 = HC05(3, 2);
 
 void setup()
 {
-  DEBUG_BEGIN(9600);
+  DEBUG_BEGIN(19200);
   DEBUG_PRINTLN("Setup");
-  delay(3000);
+  delay(3000);  // this delay is for debugging convenience only
   DEBUG_PRINTLN("DelayComplete");
   myHC05.findBaud();
-  myHC05.cmd("AT");
-  /*
-  Serial.write("Setting baud");
-  myHC05.cmd("AT+UART=9600,0,0");
-  delay(100);
-  Serial.write("now reset 1 ");
-  myHC05.cmd("AT+RESET");
-  //digitalWrite(5, LOW);
-  delay(1000);
-  //digitalWrite(5, HIGH);
-  Serial.write("AT #2 ");
-  Serial.println(myHC05.cmd("AT"));
-  Serial.write("New Baud 9600??");
-  Serial.println(myHC05.findBaud());
-  */
-
 }
 
 void loop()
 {
   myHC05.setBaud(19200);
   myHC05.cmd("AT+STATE?");
-  delay(1000);
-  myHC05.cmd("AT+STATE?");
-  /*
-  delay(1000);
-  myHC05.cmd("AT+STATE?");
-  delay(1000);
-  myHC05.cmd("AT+STATE?");
-  delay(1000);
-  myHC05.cmd("AT+STATE?");
-  delay(1000);
-  myHC05.cmd("AT+STATE?");
-  delay(1000);
-  myHC05.cmd("AT+STATE?");
-  delay(1000);
-  myHC05.cmd("AT+STATE?");
-  delay(1000);
-  myHC05.cmd("AT+STATE?");
-  delay(1000);
-  myHC05.write("9600\r\n");
-  delay(1000);
-/*  myHC05.setBaud(19200);
-  myHC05.write("19200\r\n");
+  myHC05.println("19200");
+
+  delay(1000); // Allow buffer to drain
   myHC05.setBaud(38400);
-  myHC05.write("38400\r\n");
+  myHC05.cmd("AT+STATE?");
+  myHC05.println("38400");
+
+  delay(1000); // Allow buffer to drain
   myHC05.setBaud(57600);
-  myHC05.write("57600\r\n");
+  myHC05.cmd("AT+STATE?");
+  myHC05.println("57600");
+  /*
+   * UNO does not reliably read from HC-05 at 115.2k
+   */
+  //delay(1000); // Allow buffer to drain
   //myHC05.setBaud(115200);
-  //myHC05.write("115200\r\n");
-  */
-  delay(1000);
+  //myHC05.cmd("AT+STATE?");
+  //myHC05.println("115200");
+  
   for(int i = 0; i<10; i++)
   {
     //myHC05.cmd("AT");
     myHC05.write("got here\r\n");
-    myHC05.write(i);
+    myHC05.println(i);
     delay(1000);
   }
   for (int i = 0; true; i++)
